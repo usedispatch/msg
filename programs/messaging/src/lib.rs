d8f9859d314677b1b2f9edeffc181e3340520741
Vyacheslav Akhmechet <coffeemug@gmail.com>
2021-12-04 23:55:14 -0800

    Initial commit

---
+++ programs/messaging/src/lib.rs
@@ -0,0 +1,104 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+const _PROTOCOL_SEED: &[u8] = "messaging".as_bytes();
+const _MAILBOX_SEED: &[u8] = "mailbox".as_bytes();
+const _MESSAGE_SEED: &[u8] = "message".as_bytes();
+
+#[program]
+pub mod messaging {
+    use super::*;
+    /// Send a message to the receiver. Note that anyone can create a mailbox for the receiver
+    /// and send messages.
+    pub fn send_message(ctx: Context<SendMessage>, text: String, url: String) -> ProgramResult {
+        let mailbox = &mut ctx.accounts.mailbox;
+        mailbox.message_count = mailbox.message_count + 1;
+
+        let message = &mut ctx.accounts.message;
+        message.sender = ctx.accounts.payer.key().clone();
+        message.text = text;
+        message.url = url;
+
+        Ok(())
+    }
+
+    /// Close the next message account and send rent to the receiver. Note, only
+    /// the receiver can do this.
+    pub fn close_message(ctx: Context<CloseMessage>) -> ProgramResult {
+        let mailbox = &mut ctx.accounts.mailbox;
+        mailbox.read_message_count = mailbox.read_message_count + 1;
+
+        if mailbox.read_message_count > mailbox.message_count {
+            return Err(ProgramError::InvalidArgument);
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(text: String, url: String)]
+pub struct SendMessage<'info> {
+    #[account(init_if_needed,
+        payer = payer,
+        seeds = [_PROTOCOL_SEED, _MAILBOX_SEED, receiver.key().as_ref()],
+        bump,
+    )]
+    pub mailbox: Account<'info, Mailbox>,
+    pub receiver: AccountInfo<'info>,
+
+    #[account(init,
+        payer = payer,
+        space =
+            8                               // account discriminator
+            + 32                            // sender pubkey
+            + 4 + text.as_bytes().len()     // text string
+            + 4 + url.as_bytes().len(),     // url string
+        seeds = [_PROTOCOL_SEED, _MESSAGE_SEED, receiver.key().as_ref(), &mailbox.message_count.to_le_bytes()],
+        bump,
+    )]
+    pub message: Account<'info, Message>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CloseMessage<'info> {
+    #[account(mut,
+        seeds = [_PROTOCOL_SEED, _MAILBOX_SEED, receiver.key().as_ref()],
+        bump,
+    )]
+    pub mailbox: Account<'info, Mailbox>,
+    pub receiver: Signer<'info>,
+
+    #[account(mut,
+        close = rent_destination,
+        seeds = [_PROTOCOL_SEED, _MESSAGE_SEED, receiver.key().as_ref(), &mailbox.read_message_count.to_le_bytes()],
+        bump,
+    )]
+    pub message: Account<'info, Message>,
+
+    #[account(mut)]
+    pub rent_destination: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Mailbox {
+    pub read_message_count: u32,
+    pub message_count: u32,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Message {
+    pub sender: Pubkey,
+    pub text: String,
+    pub url: String,
+}
---
+++ tests/messaging.ts
@@ -0,0 +1,151 @@
+import * as anchor from '@project-serum/anchor';
+import { strict as assert } from 'assert';
+import { Program } from '@project-serum/anchor';
+import { Messaging } from '../target/types/messaging';
+
+/*
+  Wrapping below in a client library TBD
+  Should be really simple. Smtg like this:
+
+  ```
+  // `receiver` can be a pubkey, or a keypair (if keypair, `pop` can be called)
+  const mailbox = new Mailbox(conn, receiver);
+
+  // Send messages like this (`payer` must sign):
+  await mailbox.send("text0", "url0", payer);
+  await mailbox.send("text1", "url1", payer);
+
+  // Read messages like this (nobody has to sign)
+  // Returns all new messages that haven't been popped
+  const messages = await mailbox.fetch();
+
+  // Close messages FIFO and return rent (receiver must sign)
+  await mailbox.pop();
+  await mailbox.pop();
+  ```
+*/
+
+describe('messaging', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Messaging as Program<Messaging>;
+
+  it('Basic test', async () => {
+    const conn = anchor.getProvider().connection;
+  
+    const receiver = anchor.web3.Keypair.generate();
+
+    const payer = anchor.web3.Keypair.generate();
+    await conn.confirmTransaction(await conn.requestAirdrop(payer.publicKey, 2 * anchor.web3.LAMPORTS_PER_SOL));
+
+    // send a couple of messages
+    const [mailbox] = await anchor.web3.PublicKey.findProgramAddress([
+      Buffer.from("messaging"),
+      Buffer.from("mailbox"),
+      receiver.publicKey.toBuffer(),
+    ], program.programId)
+
+    // Send first message
+    const msgCountBuf0 = Buffer.allocUnsafe(4);
+    msgCountBuf0.writeInt32LE(0);
+    const [message0] = await anchor.web3.PublicKey.findProgramAddress([
+      Buffer.from("messaging"),
+      Buffer.from("message"),
+      receiver.publicKey.toBuffer(),
+      msgCountBuf0,
+    ], program.programId);
+
+    const tx0 = await program.rpc.sendMessage("text0", "url0", {
+      accounts: {
+        mailbox: mailbox,
+        receiver: receiver.publicKey,
+        message: message0,
+        payer: payer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [
+        payer,
+      ],
+    });
+    await conn.confirmTransaction(tx0);
+
+    // Send second message
+    const msgCountBuf1 = Buffer.allocUnsafe(4);
+    msgCountBuf1.writeInt32LE(1);
+    const [message1] = await anchor.web3.PublicKey.findProgramAddress([
+      Buffer.from("messaging"),
+      Buffer.from("message"),
+      receiver.publicKey.toBuffer(),
+      msgCountBuf1,
+    ], program.programId);
+
+    const tx1 = await program.rpc.sendMessage("text1", "url1", {
+      accounts: {
+        mailbox: mailbox,
+        receiver: receiver.publicKey,
+        message: message1,
+        payer: payer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [
+        payer,
+      ],
+    });
+    await conn.confirmTransaction(tx1);
+
+    // assert mailbox and messages look good
+    let mailboxAccount = await program.account.mailbox.fetch(mailbox);
+    assert.ok(mailboxAccount.messageCount === 2);
+    assert.ok(mailboxAccount.readMessageCount === 0);
+
+    const messageAccount0 = await program.account.message.fetch(message0);
+
+    assert.ok(messageAccount0.sender.equals(payer.publicKey))
+    assert.ok(messageAccount0.text === "text0");
+    assert.ok(messageAccount0.url === "url0");
+    
+    const messageAccount1 = await program.account.message.fetch(message1);
+    assert.ok(messageAccount1.sender.equals(payer.publicKey))
+    assert.ok(messageAccount1.text === "text1");
+    assert.ok(messageAccount1.url === "url1");
+
+    // close messages
+    const tx2 = await program.rpc.closeMessage({
+      accounts: {
+        mailbox: mailbox,
+        receiver: receiver.publicKey,
+        message: message0,
+        rentDestination: receiver.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [
+        receiver,
+      ],
+    });
+    await conn.confirmTransaction(tx2);
+
+    const tx3 = await program.rpc.closeMessage({
+      accounts: {
+        mailbox: mailbox,
+        receiver: receiver.publicKey,
+        message: message1,
+        rentDestination: receiver.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [
+        receiver,
+      ],
+    });
+    await conn.confirmTransaction(tx3);
+
+    // assert mailbox looks good and rent was returned
+    mailboxAccount = await program.account.mailbox.fetch(mailbox);
+    assert.ok(mailboxAccount.messageCount === 2);
+    assert.ok(mailboxAccount.readMessageCount === 2);
+
+    const receiverBalance = await conn.getBalance(receiver.publicKey);
+    assert.ok(receiverBalance !== 0);
+  });
+});
