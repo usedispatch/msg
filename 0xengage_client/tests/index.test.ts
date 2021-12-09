02408eaff16e54f839544af476a661be81cefe82
viksit <viksit@usedispatch.xyz>
2021-12-08 17:42:49 -0800

    some basic things

--- 0xengage_client/tests/index.test.ts
+++ 0xengage_client/tests/index.test.ts
@@ -1,9 +1,34 @@
+import { Keypair, clusterApiUrl } from '@solana/web3.js';
+import * as web3 from '@solana/web3.js';
+import * as bs58 from 'bs58';
+import { Mailbox } from "../src/";
+
+
+const walletSecretKey = process.env.WALLET_SECRET_KEY!;
+const conn = new web3.Connection(web3.clusterApiUrl('devnet'));
+const payer = Keypair.fromSecretKey(bs58.decode(walletSecretKey));
+const receiver = Keypair.generate();
+
+
 describe("Test for initial Jest setup.", () => {
     describe("practiceTest", () => {
-        test("Given 'Hello World!', return 'Hello World!'", () => {
+        test("Given 'Hello World!', return 'Hello World!'", async () => {
+            console.log(`wallet secret key ${walletSecretKey}`);
             const received = "Hello World!";
             const expected = "Hello World!";
             expect(received).toBe(expected);
+
+            console.log('receiver', receiver.publicKey);
+            console.log('payer', payer.publicKey);
+
+            const mailbox = new Mailbox(conn, {
+                receiver, payer,
+            });
+
+            await mailbox.send("text0");
+            await mailbox.send("text1");
+
+
         });
     });
 
