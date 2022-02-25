fce772d58e12462c8e0d5eabe735a9c6d249ad8d
fastestmannr <msfasman@hotmail.com>
2022-02-24 21:22:31 -0500

    Revert "Use the type already defined by anchor"
    This reverts commit d30f8960f448f53e83f86560c9b8070c5a41de14.

--- usedispatch_client/src/index.ts
+++ usedispatch_client/src/index.ts
@@ -39,8 +39,16 @@ export type MailboxOpts = MailboxReceiver &
   MailboxPayer &
   MailboxSender & {
     skipAnchorProvider?: boolean;
-    wallet?: anchor.Wallet;
+    wallet?: WalletInterface;
   };
+
+export interface WalletInterface {
+    signTransaction(tx: anchor.web3.Transaction): Promise<anchor.web3.Transaction>;
+  
+    signAllTransactions(txs: anchor.web3.Transaction[]): Promise<anchor.web3.Transaction[]>;
+  
+    get publicKey(): anchor.web3.PublicKey;
+}
 
 export class Mailbox {
   public receiverAddress: anchor.web3.PublicKey;
