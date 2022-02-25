686681b69908e1d2d5ca6572093955fb437dfbaf
fastestmannr <msfasman@hotmail.com>
2022-02-25 00:39:46 -0500

    Change mailbox to belong to a single address but send to any number of addresses

--- usedispatch_client/tests/index.test.ts
+++ usedispatch_client/tests/index.test.ts
@@ -3,7 +3,7 @@ import * as os from 'os';
 import * as path from 'path';
 import * as web3 from '@solana/web3.js';
 import * as bs58 from 'bs58';
-import { Mailbox } from "../src/";
+import { Mailbox, KeyPairWallet } from "../src/";
 
 const getPayer = () : web3.Keypair => {
     if (process.env.WALLET_SECRET_KEY) {
@@ -19,9 +19,10 @@ const getPayer = () : web3.Keypair => {
 const conn = new web3.Connection(web3.clusterApiUrl('devnet'));
 const payer = getPayer();
 const receiver = web3.Keypair.generate();
-const mailbox = new Mailbox(conn, {
-    receiver, payer,
-});
+const payerWallet = new KeyPairWallet(payer);
+const receiverWallet = new KeyPairWallet(receiver)
+const sendMailbox = new Mailbox(conn, payerWallet);
+const receiveMailbox = new Mailbox(conn, receiverWallet);
 
 console.log('receiver', receiver.publicKey.toBase58());
 console.log('payer', payer.publicKey.toBase58());
@@ -29,17 +30,18 @@ console.log('payer', payer.publicKey.toBase58());
 describe("Test for initial Mailbox setup.", () => {
     describe("mailboxTest", () => {
         test("Mailbox Send, receive, pop test", async () => {
+            await conn.confirmTransaction(await conn.requestAirdrop(receiver.publicKey, 2 * web3.LAMPORTS_PER_SOL));
 
             console.log('Send message 1');
-            const txSig0 = await mailbox.send("text0");
+            const txSig0 = await sendMailbox.send("text0", receiver.publicKey);
             await conn.confirmTransaction(txSig0, 'finalized');
 
             console.log('Send message 2');
-            const txSig1 = await mailbox.send("text1");
+            const txSig1 = await sendMailbox.send("text1", receiver.publicKey);
             await conn.confirmTransaction(txSig1, 'finalized');
 
             console.log('Fetch messages from mailbox');
-            let messages = await mailbox.fetch();
+            let messages = await receiveMailbox.fetch();
             expect(messages.length).toEqual(2);
 
             expect(messages[0].sender.equals(payer.publicKey))
@@ -49,20 +51,20 @@ describe("Test for initial Mailbox setup.", () => {
             expect(messages[1].data).toEqual("text1");
 
             console.log('Pop 1 message from mailbox');
-            const txSig2 = await mailbox.pop();
+            const txSig2 = await receiveMailbox.pop();
             await conn.confirmTransaction(txSig2, 'finalized');
 
-            let _messages = await mailbox.fetch();
+            let _messages = await receiveMailbox.fetch();
             expect(_messages.length).toEqual(1);
 
             expect(_messages[0].sender).toEqual(payer.publicKey);
             expect(_messages[0].data).toEqual("text1");
 
             console.log('Pop 1 message from mailbox');
-            const txSig3 = await mailbox.pop();
+            const txSig3 = await receiveMailbox.pop();
             await conn.confirmTransaction(txSig3, 'finalized');
 
-            let __messages = await mailbox.fetch();
+            let __messages = await receiveMailbox.fetch();
             expect(__messages.length).toEqual(0);
 
         });
