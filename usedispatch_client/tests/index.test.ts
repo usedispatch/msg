883dd1837177036ab99c54620d0c95fd1054e102
viksit <viksit@gmail.com>
2022-04-02 19:37:46 -0700

    fix getMessageId to use receiveraddress

--- usedispatch_client/tests/index.test.ts
+++ usedispatch_client/tests/index.test.ts
@@ -42,18 +42,19 @@ describe("Test for initial Mailbox setup.", () => {
       const txSig1 = await senderMailbox.send("text1", receiver.publicKey);
       await conn.confirmTransaction(txSig1, 'finalized');
 
-      // console.log('Fetch messages from mailbox');
-      // let messages = await receiverMailbox.fetch();
-      // expect(messages.length).toEqual(2);
+      console.log('Fetch messages from mailbox');
+      let messages = await receiverMailbox.fetch();
+      expect(messages.length).toEqual(2);
 
       console.log('Fetch sent messages to receiver from senders mailbox');
-      let sentMessages = await senderMailbox.fetchSent(receiver.publicKey);
+      const sentMessages = await senderMailbox.fetchSent(receiver.publicKey);
       console.log('1. sentMessages: (should be 2)', sentMessages);
+      expect(sentMessages.length).toEqual(0);
 
       console.log('Fetch sent messages to receiver from senders mailbox');
       let sentMessages_ = await senderMailbox.fetchSent(sender.publicKey);
       console.log('2. sentMessages_ (should be 0 since theres no messages in this account):', sentMessages_);
-
+      expect(sentMessages_.length).toEqual(0);
 
       // expect(messages[0].sender.equals(sender.publicKey))
       // expect(messages[0].data).toEqual("text0");
