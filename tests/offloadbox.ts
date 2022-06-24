cf0fef42eb7f2f539fba0bb59d6fd35ed4ade15b
Andrew Lee <andrew@usedispatch.xyz>
2022-06-24 13:06:34 -0700

    feat: init offloadbox test

--- tests/offloadbox.ts
+++ tests/offloadbox.ts
@@ -1,2 +1,15 @@
 import * as anchor from '@project-serum/anchor';
 import { strict as assert } from 'assert';
+
+import { DispatchConnection, clusterAddresses } from '../usedispatch_client/src';
+
+describe('offloadbox', () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const conn = anchor.getProvider().connection;
+  const TREASURY = clusterAddresses.get('devnet').treasuryAddress;
+
+  it('Initializes an offloadbox', async () => {
+    // TODO
+  });
+});
