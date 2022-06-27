fc30286df4d8862597542f7fa79dc5d6a1c98d2b
fastestmannr <90209295+fastestmannr@users.noreply.github.com>
2022-06-27 16:12:34 -0400

    Initial work for post restrictions, need to add forum endpoints and tests (#49)
    * post restrictions
    * dependency on metaplex for NFT metadata

---
+++ programs/postbox/src/errors.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum PostboxErrorCode {
+    // Postbox initialize errors
+    #[msg("If no target string, target account must be the signer")]
+    NotPersonalPostbox,
+    #[msg("The description provided is not a description setting")]
+    BadDescriptionSetting,
+
+    // Create post errors
+    #[msg("The provided post ID is too large an increase")]
+    PostIdTooLarge = 100,
+    #[msg("The reply-to account is not a Post account")]
+    ReplyToNotPost,
+    #[msg("Replies cannot have a further reply restriction")]
+    ReplyCannotRestrictReplies,
+
+    // Post restriction errors
+    #[msg("The provided token account is not a token account")]
+    NotTokenAccount = 200,
+    #[msg("Missing the token required by the restriction")]
+    MissingTokenRestriction,
+    #[msg("Account provided is not expected metadata key")]
+    InvalidMetadataKey,
+    #[msg("The provided account is not a metadata account")]
+    MetadataAccountInvalid,
+    #[msg("No collection set on the metadata")]
+    NoCollectionOnMetadata,
+    #[msg("Missing an NFT from the collection required by the restriction")]
+    MissingCollectionNftRestriction,
+    #[msg("Cannot parse a setting")]
+    MalformedSetting,
+    #[msg("Extra account offsets invalid for this restriction type")]
+    InvalidRestrictionExtraAccounts,
+    #[msg("Must supply offsets when a post restriction applies")]
+    MissingRequiredOffsets,
+}
---
+++ programs/postbox/src/post_restrictions.rs
@@ -0,0 +1,99 @@
+use anchor_lang::prelude::*;
+use mpl_token_metadata;
+use crate::errors::PostboxErrorCode;
+
+#[derive(
+    AnchorSerialize,
+    AnchorDeserialize,
+    Clone,
+    PartialEq,
+    Eq
+)]
+pub enum PostRestrictionRule {
+    TokenOwnership { mint: Pubkey, amount: u64 },
+    NftOwnership { collection_id: Pubkey },
+    Null,
+}
+
+#[derive(
+    AnchorSerialize,
+    AnchorDeserialize,
+    Clone,
+    PartialEq,
+    Eq
+)]
+pub enum PostRestrictionAccountIndices {
+    TokenOwnership { token_idx: u8 },
+    NftOwnership { token_idx: u8, meta_idx: u8, collection_idx: u8 },
+    Null,
+}
+
+impl PostRestrictionRule {
+    pub fn get_size(&self) -> usize {
+        return match self.try_to_vec() {
+            Ok(v) => v.len(),
+            Err(_) => 0,
+        };
+    }
+
+    pub fn validate_reply_allowed(&self,
+        poster: &Pubkey,
+        extra_accounts: &[AccountInfo],
+        account_indices: &PostRestrictionAccountIndices,
+    ) -> Result<()> {
+        match self {
+            PostRestrictionRule::TokenOwnership { mint, amount } => {
+                if let PostRestrictionAccountIndices::TokenOwnership { token_idx } = account_indices {
+                    let membership_token = &extra_accounts[Into::<usize>::into(*token_idx)];
+                    let token = Account::<anchor_spl::token::TokenAccount>::try_from(membership_token).map_err(
+                        |_| Error::from(PostboxErrorCode::InvalidRestrictionExtraAccounts).with_source(source!())
+                    )?;
+                    let has_token = token.owner == *poster && token.mint == *mint && token.amount >= *amount;
+                    require!(has_token, PostboxErrorCode::MissingTokenRestriction);
+                } else {
+                    return Err(Error::from(PostboxErrorCode::InvalidRestrictionExtraAccounts).with_source(source!()));
+                }
+            },
+
+            PostRestrictionRule::NftOwnership { collection_id } => {
+                if let PostRestrictionAccountIndices::NftOwnership { token_idx, meta_idx, collection_idx } = account_indices {
+                    let membership_token = &extra_accounts[Into::<usize>::into(*token_idx)];
+                    let membership_mint_meta = &extra_accounts[Into::<usize>::into(*meta_idx)];
+                    let membership_collection = &extra_accounts[Into::<usize>::into(*collection_idx)];
+                    let token = Account::<anchor_spl::token::TokenAccount>::try_from(membership_token).map_err(
+                        |_| Error::from(PostboxErrorCode::InvalidRestrictionExtraAccounts).with_source(source!())
+                    )?;
+                    let expected_meta_key = mpl_token_metadata::pda::find_metadata_account(& token.mint).0;
+                    require!(membership_mint_meta.key() == expected_meta_key, PostboxErrorCode::InvalidMetadataKey);
+                    let mint_meta = Account::<crate::nft_metadata::Metadata>::try_from(membership_mint_meta).map_err(
+                        |_| Error::from(PostboxErrorCode::InvalidRestrictionExtraAccounts).with_source(source!())
+                    )?;
+                    require!(mint_meta.collection.is_some(), PostboxErrorCode::NoCollectionOnMetadata);
+                    let collection = mint_meta.collection.as_ref().unwrap();
+                    let has_collection_nft = token.owner == *poster
+                        && collection.verified
+                        && collection.key == *collection_id
+                        && collection.key == membership_collection.key()
+                        && *membership_collection.owner == anchor_spl::token::ID;
+                    require!(has_collection_nft, PostboxErrorCode::MissingCollectionNftRestriction);
+                } else {
+                    return Err(Error::from(PostboxErrorCode::InvalidRestrictionExtraAccounts).with_source(source!()));
+                }
+            },
+
+            PostRestrictionRule::Null => {},
+        }
+
+        Ok(())
+    }
+}
+
+pub fn get_reply_restriction_size(info: &AccountInfo) -> usize {
+    let maybe_post = Account::<crate::Post>::try_from(info);
+    if maybe_post.is_ok() {
+        if let Some(restriction) = & maybe_post.unwrap().post_restrictions {
+            return restriction.get_size();
+        }
+    }
+    return 0;
+}
