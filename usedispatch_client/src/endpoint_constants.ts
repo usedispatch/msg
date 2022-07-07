16d9ae584337c136b5b5e01d3d2cf3258b76670d
Andrew Lee <andrew@usedispatch.xyz>
2022-07-07 09:46:02 -0700

    wip

--- usedispatch_client/src/endpoint_constants.ts
+++ usedispatch_client/src/endpoint_constants.ts
@@ -4,6 +4,7 @@
  *
  * All fees in lamports
  */
+import { PublicKey } from '@solana/web3.js';
 
 /** One sol is 10e9 lamports */
 export const SOL = 10e9;
@@ -18,3 +19,8 @@ export const CREATE_OFFLOADBOX_FEE = 1.5 * SOL;
  * This number chosen relatively arbitrarily, TODO make it better
  */
 export const CREATE_POST_FEE = 30000;
+
+/**
+ * The public key of the endpoint
+ */
+export const ENDPOINT_PUBLIC_KEY = new PublicKey('8NSUiHk3tPk7bbgxfDU1ZvAG8AdQHf7fjsu43DvQLrRD');
