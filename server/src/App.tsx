6368b65cad985653bc3dd580f29d2ba712fddfe7
Andrew Lee <andrew@usedispatch.xyz>
2022-07-11 14:20:10 -0700

    indent App.tsx

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {
-    Container
+  Container
 } from 'react-bootstrap';
 import {
-    Header,
-    Content
+  Header,
+  Content
 } from './components';
 import { Wallet } from './Wallet';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -16,8 +16,8 @@ function App() {
   return (
     <Wallet>
       <BrowserRouter>
-          <Header />
-          <hr />
+        <Header />
+        <hr />
         <Routes>
           <Route path="/" element={<Content />} />
         </Routes>
