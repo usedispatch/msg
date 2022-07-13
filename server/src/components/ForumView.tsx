cc5718ef693e4dfa56679121b847039f3038bd24
Andrew Lee <andrew@usedispatch.xyz>
2022-07-13 10:00:22 -0700

    list addresses

--- server/src/components/ForumView.tsx
+++ server/src/components/ForumView.tsx
@@ -1,15 +1,65 @@
 import {
+  useEffect,
+  useState
+} from 'react';
+import {
   Container,
   Card
 } from 'react-bootstrap';
 import {
   useParams
 } from 'react-router-dom';
+import axios from 'axios'; 
+
+async function handler() {
+  const resp = await axios({
+    url: 'https://arweave.dev/graphql',
+    method: 'post',
+    data: {
+      query: `{
+    transactions(first: 160, 
+      tags: [
+        {
+          name: "App-Name",
+          values: ["PublicSquare"]
+        },
+        {
+          name: "Content-Type",
+          values: ["text/plain"]
+        },
+
+      ]
+    ) {
+      edges {
+        node {
+          id
+          data {
+            size
+          }
+          tags {
+            name,
+            value
+          }
+        }
+      }
+    }
+  }`
+    }
+  });
+
+  const edges = resp.data.data.transactions.edges as { node: { id: string }}[]
+  return edges.map(edge => edge.node.id);
+}
 
 export function ForumView() {
   const { identifier } = useParams();
 
-  const posts = ['one', 'two', 'three'];
+  useEffect(() => {
+    handler()
+      .then(ids => setPosts(ids));
+  }, []);
+
+  const [posts, setPosts] = useState<string[]>([]);
 
   return (
     <Container>
@@ -23,5 +73,5 @@ export function ForumView() {
       </Card>
       )}
     </Container>
-      );
+  );
 };
