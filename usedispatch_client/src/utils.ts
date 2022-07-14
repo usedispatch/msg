d93d69b980a3e566f1365df052855e0abc3ba3d4
Andrew Lee <andrew@usedispatch.xyz>
2022-07-14 15:06:54 -0700

    skeleton get NFTs function

---
+++ usedispatch_client/src/utils.ts
@@ -0,0 +1,13 @@
+import {
+  Connection,
+  PublicKey
+} from '@solana/web3.js';
+import {
+  TOKEN_PROGRAM_ID
+} from '@solana/spl-token';
+
+export async function getNFTsForOwner(
+  connection: Connection,
+  publicKey: PublicKey
+) {
+}
