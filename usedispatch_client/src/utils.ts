3802ba626301e179df0d93365bca4d4765719622
Andrew Lee <andrew@usedispatch.xyz>
2022-07-15 14:27:16 -0700

    feat: get tokens

--- usedispatch_client/src/utils.ts
+++ usedispatch_client/src/utils.ts
@@ -6,6 +6,10 @@ import {
 import {
   TOKEN_PROGRAM_ID
 } from '@solana/spl-token';
+import {
+  Metadata,
+  PROGRAM_ID
+} from '@metaplex-foundation/mpl-token-metadata';
 
 // export interface NFT {
 //   mint: string;
@@ -15,7 +19,7 @@ import {
 export async function getMintsForOwner(
   connection: Connection,
   publicKey: PublicKey
-): Promise<any> {
+): Promise<PublicKey[]> {
 
   const { value } = await connection.getParsedTokenAccountsByOwner(
     publicKey, { programId: TOKEN_PROGRAM_ID }
@@ -23,16 +27,48 @@ export async function getMintsForOwner(
 
   const parsedObjects = value.map(({ account }) => account.data.parsed);
   const mints = parsedObjects
-  .filter(obj => 'info' in obj && 'mint' && obj.info)
-  .map(obj => obj.info.mint)
+  .filter(obj =>
+    'info' in obj &&
+    'mint' in obj.info && 
+    typeof obj.info.mint === 'string'
+  )
+  .map(obj => new PublicKey(obj.info.mint))
 
   return mints;
+}
+
+export async function getNFTsForOwner(
+  connection: Connection,
+  publicKey: PublicKey
+): Promise<Metadata[]> {
+  const mints = await getMintsForOwner(connection, publicKey);
+
+  const derivedAddresses = await Promise.all(
+    mints.map(mint =>
+      PublicKey.findProgramAddress([
+        Buffer.from('metadata'),
+        PROGRAM_ID.toBuffer(),
+        mint.toBuffer()
+      ],
+        PROGRAM_ID
+      ).then(([derived]) => derived)
+    )
+  );
+
+  return Promise.all(
+    derivedAddresses.map(addr =>
+      Metadata.fromAccountAddress(
+        connection,
+        addr
+      )
+    )
+  );
 }
 
 async function main() {
   const conn = new Connection(clusterApiUrl('mainnet-beta'));
 
-  const nfts = await getMintsForOwner(
+  const nfts = await getNFTsForOwner(
     conn,
     new PublicKey(
       '7ycUFfnspMwnjp2DfSjAvZgf7g7T6nugrGv2kpzogrNC'
