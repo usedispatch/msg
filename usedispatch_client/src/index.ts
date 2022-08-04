fe5a4bb61c03d8de2702e7f4c7ef3006820e090b
viksit <viksit@gmail.com>
2022-08-03 17:38:04 -0700

    remove bonfida dep
    update missing reference

--- usedispatch_client/src/index.ts
+++ usedispatch_client/src/index.ts
@@ -2,7 +2,7 @@ export { DispatchConnection } from './connection';
 export { clusterAddresses, defaultCluster, seeds } from './constants';
 export { Forum, ForumInfo, ForumPost, IForum } from './forum';
 export { MailboxAccount, MessageAccount, MailboxOpts, Mailbox } from './mailbox';
-export { lookupDotSol } from './naming';
+// export { lookupDotSol } from './naming';
 export { Postbox, SettingsType, PostRestriction } from './postbox';
 export { KeyPairWallet, WalletInterface } from './wallets';
 export * from './utils';
--- usedispatch_client/src/naming.ts
+++ usedispatch_client/src/naming.ts
@@ -1,37 +1,37 @@
-import { getHashedName, getNameAccountKey, NameRegistryState } from '@bonfida/spl-name-service';
+// import { getHashedName, getNameAccountKey, NameRegistryState } from '@bonfida/spl-name-service';
 
-import * as web3 from '@solana/web3.js';
+// import * as web3 from '@solana/web3.js';
 
-/* Bonfida naming support
- */
+// /* Bonfida naming support
+//  */
 
-const ROOT_TLD_AUTHORITY = new web3.PublicKey('ZoAhWEqTVqHVqupYmEanDobY7dee5YKbQox9BNASZzU');
-const SOL_TLD_AUTHORITY = new web3.PublicKey('58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx');
+// const ROOT_TLD_AUTHORITY = new web3.PublicKey('ZoAhWEqTVqHVqupYmEanDobY7dee5YKbQox9BNASZzU');
+// const SOL_TLD_AUTHORITY = new web3.PublicKey('58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx');
 
-export type DotSolLookup = {
-  ownerPubKey: web3.PublicKey | undefined;
-};
+// export type DotSolLookup = {
+//   ownerPubKey: web3.PublicKey | undefined;
+// };
 
-export const lookupDotSol = async (conn: web3.Connection, solDomainName: string): Promise<DotSolLookup> => {
-  const hashedName = await getHashedName(solDomainName);
-  const domainKey = await getNameAccountKey(hashedName, undefined, SOL_TLD_AUTHORITY);
+// export const lookupDotSol = async (conn: web3.Connection, solDomainName: string): Promise<DotSolLookup> => {
+//   const hashedName = await getHashedName(solDomainName);
+//   const domainKey = await getNameAccountKey(hashedName, undefined, SOL_TLD_AUTHORITY);
 
-  // The retrieve method returns an object made of two fields:
-  // - registry is of type NameRegistryState
-  // - nftOwner is of type PublicKey | undefined
+//   // The retrieve method returns an object made of two fields:
+//   // - registry is of type NameRegistryState
+//   // - nftOwner is of type PublicKey | undefined
 
-  const { registry, nftOwner } = await NameRegistryState.retrieve(conn, domainKey);
+//   const { registry, nftOwner } = await NameRegistryState.retrieve(conn, domainKey);
 
-  const res: DotSolLookup = { ownerPubKey: undefined };
+//   const res: DotSolLookup = { ownerPubKey: undefined };
 
-  if (nftOwner !== undefined) {
-    // Domain is tokenized, which means the nftOwner pubkey is the owner of the domain NFT
-    // All funds and messages should be sent here to nftOwner themselves.
-    res.ownerPubKey = nftOwner;
-  } else if (nftOwner === undefined) {
-    // Domain is not tokenized, so the funds or messages should be sent to registry.owner
-    res.ownerPubKey = registry.owner;
-  }
+//   if (nftOwner !== undefined) {
+//     // Domain is tokenized, which means the nftOwner pubkey is the owner of the domain NFT
+//     // All funds and messages should be sent here to nftOwner themselves.
+//     res.ownerPubKey = nftOwner;
+//   } else if (nftOwner === undefined) {
+//     // Domain is not tokenized, so the funds or messages should be sent to registry.owner
+//     res.ownerPubKey = registry.owner;
+//   }
 
-  return res;
-};
+//   return res;
+// };
