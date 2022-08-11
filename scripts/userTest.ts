c9ce52822ecbf35796710bd669dc72a0695f6b88
Zayyan Faizal <zayyan@usedispatch.xyz>
2022-08-11 10:58:50 -0500

    add several topics

--- scripts/userTest.ts
+++ scripts/userTest.ts
@@ -8,19 +8,29 @@ import {
   } from "../usedispatch_client/src";
 
 const TRANSACTION_TIMEOUT = 500;
-const COLLECTION_ID = "Fs5wSa7GYtTqivXGqHyx673v5oPuD5Cb7ij9utsFKdLb" // dispatch dev forum
+const COLLECTION_ADDRESS = "8gbq3z1ySWYNt7GDZrnr8CAxCfsvtQSsZr2NuGu5DWB7" // stress test forum
+const NUMBER_OF_TOPICS = 1;
+const NUMBER_OF_POSTS = 500
+
+//custom topic data
+const TOPIC_ID = 136
+const TOPIC_ADDRESS = "H98AqMzvsssyoA5qWEjVSwGhyFWbF3nd2AHWEr5aESY1"
+const TOPIC_POSTER = "9X8tGwnZcfmmchJpXi3j7ENVvVD919prYSzMGudhFava"
+const TOPIC_DATE = "2022-08-11T05:18:04.000Z"
+
 const TOPIC_DATA = { 
     subj: "scripted topic",
     body: "this is a scripted topic"
 }
 
+
 async function main() {
-    console.log("Starting using collection id: ", COLLECTION_ID);
+    console.log("Starting using collection id: ", COLLECTION_ADDRESS);
     const connection = new anchor.web3.Connection("http://api.devnet.solana.com", "confirmed"); 
     const wallet = new anchor.Wallet(anchor.web3.Keypair.generate());
     const dispatchConnection = new DispatchConnection(connection, wallet);
 
-    const collectionKey = new anchor.web3.PublicKey(COLLECTION_ID);
+    const collectionKey = new anchor.web3.PublicKey(COLLECTION_ADDRESS);
     const dispatchForum = new Forum(dispatchConnection, collectionKey);
 
     const TOPIC_POST = {
@@ -28,40 +38,53 @@ async function main() {
             dispatch: dispatchConnection,
             target: {}
         }, 
-        address: new anchor.web3.PublicKey("GixW8PFnhLxs9yJEbcfZzZrk66HwXAtcPFHx6JapdaQ5"),
-        postId: 86,
-        poster: new anchor.web3.PublicKey("iDPqXWr1KeDNUHQFmjuckjNgyfwtRhe2bcDvWBao5XY"),
+        address: new anchor.web3.PublicKey(TOPIC_ADDRESS),
+        postId: TOPIC_ID,
+        poster: new anchor.web3.PublicKey(TOPIC_POSTER),
         data: {
             subj: 'scripted topic',
             body: 'this is a scripted topic',
-            ts: new Date("2022-08-01T20:34:32.000Z"),
+            ts: new Date(TOPIC_DATE),
             meta: { topic: true }
         },
-        isTopic: true
+        isTopic: true,
     } as ForumPost
+
 
     await connection.confirmTransaction(await connection.requestAirdrop(wallet.publicKey, 2 * anchor.web3.LAMPORTS_PER_SOL));
     console.log("airdrop complete")
     // create topic
-    // await dispatchForum.createTopic(TOPIC_DATA);
-    // console.log("topic created")
-    // const topics = await dispatchForum.getTopicsForForum();
-    // const topicPost = topics[0]
-    // console.log(topicPost.parent, topicPost.address.toBase58(), topicPost.poster.toBase58())
-    // console.log("topic post created at ID: ", topicPost.postId, topicPost);
+    for (let i = 0; i < NUMBER_OF_TOPICS; i++) {
+        // const solBalance = await connection.getBalance(wallet.publicKey)
+        // console.log(solBalance)
+        // if ( solBalance < 10000) {
+        //     await connection.confirmTransaction(await connection.requestAirdrop(wallet.publicKey, 2 * anchor.web3.LAMPORTS_PER_SOL));
+        // }
+
+        // comment lines below to use custom topic above
+        // await dispatchForum.createTopic(TOPIC_DATA);
+        // console.log("topic created")
+        // const topics = await dispatchForum.getTopicsForForum();
+        // const TOPIC_POST = topics[0]
+        // console.log(TOPIC_POST.parent, TOPIC_POST.address.toBase58(), TOPIC_POST.poster.toBase58())
+        // console.log("topic post created at ID: ", TOPIC_POST.postId);
     
-    console.log('Start Posts')
-    for (let i = 0; i < 100; i++) {
-        const postData = {body: `p${i}`}
-        let postTxn = await dispatchForum.createForumPost(postData, TOPIC_POST);
-        console.log("created post", postData, postTxn)
-        let posts = await dispatchForum.getTopicMessages(TOPIC_POST);
-        let lastPost = posts[posts.length - 1]
-        await new Promise(f => setTimeout(f, TRANSACTION_TIMEOUT));
-        const replyData = {body: `r1`}
-        let replyTxn = await dispatchForum.replyToForumPost(lastPost, replyData);
-        console.log("created reply", replyData, replyTxn)
-        await new Promise(f => setTimeout(f, TRANSACTION_TIMEOUT));
+        console.log('Start Posts')
+        for (let i = 0; i < NUMBER_OF_POSTS/2; i++) {
+            const postData = {body: `p${i}`}
+            let postTxn = await dispatchForum.createForumPost(postData, TOPIC_POST);
+            await connection.confirmTransaction(postTxn, "finalized");
+            console.log("created post", postTxn)
+            let posts = await dispatchForum.getTopicMessages(TOPIC_POST);
+            let lastPost = posts[posts.length - 1]
+            // await new Promise(f => setTimeout(f, TRANSACTION_TIMEOUT));
+            const replyData = {body: `r1`}
+            let replyTxn = await dispatchForum.replyToForumPost(lastPost, replyData);
+            await connection.confirmTransaction(replyTxn, "finalized");
+            console.log("created reply", replyData, replyTxn)
+            // await new Promise(f => setTimeout(f, TRANSACTION_TIMEOUT));
+
+        }
     }
 }
 
