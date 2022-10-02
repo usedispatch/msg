4a9d7b5464f45722f288efd0f2c6907fd61268d1
Zayyan Faizal <zayyan@usedispatch.xyz>
2022-10-01 19:42:47 -0500

    add rpc calls

--- usedispatch_client/src/api.ts
+++ usedispatch_client/src/api.ts
@@ -2,12 +2,6 @@ const SUPABASE_URL = 'https://aiqrzujttjxgjhumjcky.supabase.co';
 const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcXJ6dWp0dGp4Z2podW1qY2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE0NzA5NjgsImV4cCI6MTk3NzA0Njk2OH0.qyDrAwwq1pyys4t12Klp7YWHCV05YRMj29Du2xRLKe8';
 import {createClient} from "@supabase/supabase-js";
-  // const axios = require('axios').default;
-
-// axios.defaults.headers.common['Authorization'] = `Bearer ${SUPABASE_ANON_KEY}`;
-// axios.defaults.headers.common['Content-Type'] = 'application/json';
-
-// axios.defaults.baseURL = SUPABASE_URL;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
@@ -22,19 +16,13 @@ export const getMaxChildId = async (cluster: string, forumID: string): Promise<n
   }
 
 
-  const {data, error} = await supabase.from(`postbox_post_id_${requestBody.clusterName}`)
+  const {data, error} = await supabase.from(`ro_view_postbox_post_id_${requestBody.clusterName}`)
     .select("*")
     .eq('forum_id', forumID)
     .limit(1)
     .single()
   
   return data.max_child_id
-
-  // edge function
-  // const request = await axios.post(`/getMaxChildId`, requestBody).catch((error: any) => {
-  //   console.log(error);
-  // });
-  // return request.data['max_child_id'];
 };
 
 export const updateAndGetNewChildId = async (cluster: string, forumID: string): Promise<number> => {
@@ -46,12 +34,8 @@ export const updateAndGetNewChildId = async (cluster: string, forumID: string): 
   if (cluster === 'mainnet-beta') {
     requestBody.clusterName = 'mainnet';
   }
-  const { data, error } = await supabase.rpc(`increment_${requestBody.clusterName}`, { forum_id_key: forumID })
+  const { data, error } = await supabase.rpc(`increment_max_child_${requestBody.clusterName}`, { forum_id_key: forumID })
   return data;
-  // const request = await axios.post(`/getNewChildId`, requestBody).catch((error: any) => {
-  //   console.log(error);
-  // });
-  // return request.data['max_child_id'];
 };
 
 export const addNewPostbox = async (cluster: string, forumID: string): Promise<void> => {
@@ -63,16 +47,8 @@ export const addNewPostbox = async (cluster: string, forumID: string): Promise<v
   if (cluster === 'mainnet-beta') {
     requestBody.clusterName = 'mainnet';
   }
-  const { data, error } = await supabase.from(`postbox_post_id_${requestBody.clusterName}`).insert(
-    [{ 
-      forum_id: forumID,
-      max_child_id: 0
-    }]
-  )
+
+  await supabase.rpc(`add_postbox_${requestBody.clusterName}`, { forum_id_key: forumID });
 
   return;
-  // const request = await axios.post(`/addNewPostbox`, requestBody).catch((error: any) => {
-  //   console.log(error);
-  // });
-  // return request;
 };
